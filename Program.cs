dc63f153ee6ed7bbd2498673a1fff0671970cd2b
Dell <Dell@DESKTOP-TK7GSO1>
2025-06-25 19:08:09 -0300

    feat: docker

--- Program.cs
+++ Program.cs
@@ -23,7 +23,9 @@ namespace StudentCenterAuthApi
                     builder.WithOrigins("https://studentcenterweb-f9bucggxcjcrescj.brazilsouth-01.azurewebsites.net", 
                                         "https://studentcenteracademic-b8b6ajakhcg4dsbx.brazilsouth-01.azurewebsites.net",
                                         "https://localhost:7260",
-                                        "https://localhost:7236")
+                                        "https://localhost:7236",
+                                        "http://localhost:5001",
+                                        "https://localhost:7291")
                            .AllowCredentials()
                            .AllowAnyHeader()
                            .AllowAnyMethod();
